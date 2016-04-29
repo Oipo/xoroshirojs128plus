47b1a38ef5ae913eb56e9672ac44a362d59ed5e7
Michael de Lang <kingoipo@gmail.com>
2016-04-29 21:17:14 +0200

    Add seed from current time

--- xoroshiro.js
+++ xoroshiro.js
@@ -4,6 +4,11 @@ exports.seed = function (high, low) {
     return xoroshirojsBinding.seed(high, low);
 }
 
+exports.seedWithCurrentTime = function () {
+    var ms = (new Date).getTime();
+    return xoroshirojsBinding.seed(ms%1000, ms);
+}
+
 exports.next = function () {
     return xoroshirojsBinding.next();
 }
